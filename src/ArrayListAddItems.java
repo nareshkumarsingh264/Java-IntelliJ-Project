73f47fb40abef94f8ad21e859b63ba59f254971c
nareshkumarsingh264 <69247085+nareshkumarsingh264@users.noreply.github.com>
2021-05-15 14:02:18 +0530

    code update successfully

---
+++ src/ArrayListAddItems.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class ArrayListAddItems {
+    public static void main(String[]args) {
+        ArrayList<String> cars=new ArrayList<String>();
+        cars.add("Bolero");
+        cars.add("Swift");
+        cars.add("Artiga");
+        cars.add("Renolt");
+        System.out.println(cars);
+
+
+    }
+}
