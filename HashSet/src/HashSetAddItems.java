484416f3e1cbc9963a47da46a3223848bfee3542
nareshkumarsingh264 <69247085+nareshkumarsingh264@users.noreply.github.com>
2021-05-16 15:50:06 +0530

    code run successfuly

---
+++ HashSet/src/HashSetAddItems.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+public class HashSetAddItems {
+    public static void main(String[] args) {
+        HashSet<String> bikes = new HashSet<String>();
+        bikes.add("Hero");
+        bikes.add("Bajaj");
+        bikes.add("Mahindra");
+        bikes.add("Honda");
+        System.out.println(bikes);
+    }
+}
