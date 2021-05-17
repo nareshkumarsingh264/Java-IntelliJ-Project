f91782ea6a631db0410feebfa82693cf8fad9640
nareshkumarsingh264 <69247085+nareshkumarsingh264@users.noreply.github.com>
2021-05-17 13:25:08 +0530

    code update

---
+++ Dte And Time/src/DisplayCurrentDateandTime.java
@@ -0,0 +1,7 @@
+import java.time.LocalDateTime;
+public class DisplayCurrentDateandTime {
+    public static void main(String[] args) {
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println(myObj);
+    }
+}
