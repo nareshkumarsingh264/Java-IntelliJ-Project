bde48b2ba9d0b0b27151557b986c27f5da34a59f
nareshkumarsingh264 <69247085+nareshkumarsingh264@users.noreply.github.com>
2021-05-20 23:31:25 +0530

    code updated

---
+++ ForLoop/src/ForEachLoop.java
@@ -0,0 +1,8 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] cars ={"Bolero","BNW","Centro","Swift"};
+        for(String i : cars){
+            System.out.println(i);
+        }
+    }
+}
