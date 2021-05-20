75c16c9b287dcf07cead157d2e202a77681a81d6
nareshkumarsingh264 <69247085+nareshkumarsingh264@users.noreply.github.com>
2021-05-20 22:33:38 +0530

    updated

---
+++ IfElse/src/If.java
@@ -0,0 +1,7 @@
+public class If {
+    public static void main(String[] args) {
+        if (18 > 16) {
+            System.out.println("18 grater then 16");
+        }
+    }
+}
---
+++ IfElse/src/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int x = 12;
+        if (x % 2 == 0) {
+            System.out.println("Even ");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
