220caae9b8c0a45f7b6a87796f58a1daaedd99c6
nareshkumarsingh264 <69247085+nareshkumarsingh264@users.noreply.github.com>
2021-05-21 13:24:43 +0530

    code run

---
+++ Arrays/src/ArrayForEach.java
@@ -0,0 +1,8 @@
+public class ArrayForEach {
+    public static void main(String[] args) {
+        String[] cars = {"Bolero", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
